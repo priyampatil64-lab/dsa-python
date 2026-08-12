ecd1cdb21f9e09d3d45a5bb11cd6051b7fa34922
Priya M Patil <priyampatil64@gmail.com>
2026-08-12 11:44:40 +0530

    Add Two Sum function implementation
    Implement the Two Sum algorithm to find indices of two numbers that add up to a target.

---
+++ 0001_two_sum/solution.py
@@ -0,0 +1,19 @@
+# 1. Two Sum
+# https://leetcode.com/problems/two-sum/
+
+def two_sum(nums, target):
+    seen = {}  # value -> index
+
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+
+    return []  # no solution found
+
+
+if __name__ == "__main__":
+    print(two_sum([2, 7, 11, 15], 9))   # [0, 1]
+    print(two_sum([3, 2, 4], 6))        # [1, 2]
+    print(two_sum([3, 3], 6))           # [0, 1]
