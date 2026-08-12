772051f76feebd3cb3b23b7791f5ff461fc9a1dc
Priya M Patil <priyampatil64@gmail.com>
2026-08-12 12:42:42 +0530

    Refactor twoSum method to remove type hints

--- day01_arrays_pt/two_sum.py
+++ day01_arrays_pt/two_sum.py
@@ -1,5 +1,8 @@
+# 1. Two Sum
+# https://leetcode.com/problems/two-sum/
+
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         seen = {}                          # empty dictionary to remember numbers we've seen
         for i, num in enumerate(nums):     # i = index, num = value, at each step
             complement = target - num      # the number we'd need to reach target
@@ -7,3 +10,10 @@ class Solution:
                 return [seen[complement], i]   # yes! return both indices
             else:
                 seen[num] = i              # no, so remember this number's index
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2, 7, 11, 15], 9))   # [0, 1]
+    print(sol.twoSum([3, 2, 4], 6))        # [1, 2]
+    print(sol.twoSum([3, 3], 6))           # [0, 1]
